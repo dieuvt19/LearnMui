fc8a3ee9c0efe6b75c9c5f52d23a9e4eefc6364f
Dieu Dieu <vothuydieu1903@gmail.com>
2022-12-29 22:26:12 +0700

    custom components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import { Stack } from "@mui/material";
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+import { margin } from "@mui/system";
 import "./App.css";
-import Leftcontent from "./components/Leftcontent";
-import Maincontent from "./components/Maincontent";
-import Rightcontent from "./components/Rightcontent";
 
 function App() {
+  const MyButton = styled(Button)({
+    backgroundColor: "#009900",
+    margin: 5,
+    color: "#ffffff",
+    "&:hover": {
+      backgroundColor: "#339966",
+    },
+  });
   return (
     <div className="App">
-      <h1>Flexbox</h1>
-      <Stack direction="row" justifyContent="space-between">
-        <Leftcontent />
-        <Maincontent />
-        <Rightcontent />
-      </Stack>
+      <h1>Custom Components</h1>
+      <MyButton>Submit</MyButton>
+      <MyButton>Register</MyButton>
+      <MyButton>Log in</MyButton>
     </div>
   );
 }
