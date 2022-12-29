78cfd262c9b33720feac8c497d3104c26efa0201
Dieu Dieu <vothuydieu1903@gmail.com>
2022-12-29 22:18:08 +0700

    breakpoints xs sm md

--- src/components/Rightcontent.js
+++ src/components/Rightcontent.js
@@ -3,7 +3,11 @@ import React from "react";
 
 function Rightcontent() {
   return (
-    <Box bgcolor="lightgreen" flex={2}>
+    <Box
+      bgcolor="lightgreen"
+      flex={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
       <h2>Rightcontent</h2>
       <p>This is the right content of the page</p>
     </Box>
