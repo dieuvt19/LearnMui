5089f6ed4cdc5a60e087b6042056911c5a581f86
Dieu Dieu <vothuydieu1903@gmail.com>
2022-12-30 22:22:48 +0700

    validation error

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "@mui/material/Container";
 import { Typography, TextField, Button, Box } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
 function Create() {
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name && details) {
+      console.log(name, details);
+    }
+
+    if (name === "") {
+      setNameError(true);
+    }
+
+    if (details === "") {
+      setDetailsError(true);
+    }
+  };
+
   return (
     <Container maxWidth="lg">
       <Typography variant="h3" align="center" gutterBottom>
         Create a new customer
       </Typography>
-      <form action="" noValidate autoComplete="off">
+      <form action="" noValidate autoComplete="off" onSubmit={handleSubmit}>
         <Box pb={2}>
           <TextField
             variant="standard"
             fullWidth
             required
             label="Name"
-            // value={}
-            // onChange={}
+            // value={name}
+            onChange={(e) => setName(e.target.value)}
+            error={nameError}
           />
           <TextField
             variant="standard"
@@ -26,7 +47,8 @@ function Create() {
             multiline
             rows={3}
             // value={}
-            // onChange={}
+            onChange={(e) => setDetails(e.target.value)}
+            error={detailsError}
           />
         </Box>
         <Button
