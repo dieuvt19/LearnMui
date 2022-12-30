eb44ae1375576d4538f4da9abfe62c7a3ee0a98c
Dieu Dieu <vothuydieu1903@gmail.com>
2022-12-30 21:12:11 +0700

    theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import styled from "@emotion/styled";
 import { Button } from "@mui/material";
-import { margin } from "@mui/system";
 import "./App.css";
 
 function App() {
-  const MyButton = styled(Button)({
-    backgroundColor: "#009900",
-    margin: 5,
-    color: "#ffffff",
-    "&:hover": {
-      backgroundColor: "#339966",
-    },
-  });
   return (
     <div className="App">
-      <h1>Custom Components</h1>
-      <MyButton>Submit</MyButton>
-      <MyButton>Register</MyButton>
-      <MyButton>Log in</MyButton>
+      <Button color="myColor">Submit</Button>
     </div>
   );
 }
