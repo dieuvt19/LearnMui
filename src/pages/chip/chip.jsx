f8e5613aa4389c240f7d60dda53f0fbc070727d4
Dieu Dieu <vothuydieu1903@gmail.com>
2022-12-30 21:50:06 +0700

    map chip and onDelete

--- src/pages/chip/chip.jsx
+++ src/pages/chip/chip.jsx
@@ -1,18 +1,36 @@
 import { Chip } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "./chip.css";
 
 function ChipExample() {
+  const [languages, setLanguages] = useState([
+    { id: 1, label: "English" },
+    { id: 2, label: "Spanish" },
+    { id: 3, label: "French" },
+    { id: 4, label: "Germany" },
+    { id: 5, label: "Italian" },
+  ]);
+  console.log(languages);
+
+  const handleDelete = (languageToDelete) => {
+    setLanguages((languages) =>
+      languages.filter((language) => language.id !== languageToDelete.id)
+    );
+  };
+
   return (
     <div>
       <h1>Chip Component</h1>
       <div className="chip-container">
-        <Chip label="My first Chip" />
-        <Chip label="My second Chip" variant="outlined" />
-        <Chip label="My third Chip" color="primary" />
-        <Chip label="My four Chip" color="success" />
-        <Chip label="My five Chip" color="info" />
-        <Chip label="My sixth Chip" color="warning" />
+        {languages.map((language) => (
+          <Chip
+            key={language.id}
+            label={language.label}
+            onDelete={() => {
+              handleDelete(language);
+            }}
+          />
+        ))}
       </div>
     </div>
   );
