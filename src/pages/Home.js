8b3e24d071e12b5a7a93e545f7077bfb79cd82ab
Dieu Dieu <vothuydieu1903@gmail.com>
2023-01-01 20:33:12 +0700

    Drawer navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
 import Container from "@mui/material/Container";
-import { Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import CustomerCard from "../components/CustomerCard";
 import { useContext } from "react";
 import CustomerContext from "../context/CustomerContext";
+import InboxIcon from "@mui/icons-material/Inbox";
 
 function Home() {
   const { customers } = useContext(CustomerContext);
@@ -12,14 +22,30 @@ function Home() {
       <Typography variant="h3" gutterBottom align="center">
         App to manage customers
       </Typography>
-      <Grid container p={5} spacing={5}>
-        {customers &&
-          customers.map((customer) => (
-            <Grid item xs={4} key={customer.id}>
-              <CustomerCard customer={customer} />
-            </Grid>
-          ))}
-      </Grid>
+      <Box sx={{ display: "flex" }}>
+        <Box>
+          <Drawer variant="permanent" anchor="left">
+            <List>
+              <ListItem component="a" href="/create">
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Create new customer" />
+              </ListItem>
+            </List>
+          </Drawer>
+        </Box>
+        <Box component="main" sx={{ flexGrow: 1, padding: 3 }}>
+          <Grid container p={5} spacing={5}>
+            {customers &&
+              customers.map((customer) => (
+                <Grid item xs={4} key={customer.id}>
+                  <CustomerCard customer={customer} />
+                </Grid>
+              ))}
+          </Grid>
+        </Box>
+      </Box>
     </Container>
   );
 }
