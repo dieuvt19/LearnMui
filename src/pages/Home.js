818b959ff39a248e94da2d5cf4718b42d69ab72e
Dieu Dieu <vothuydieu1903@gmail.com>
2023-01-01 18:38:42 +0700

    grid paper

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,44 @@
-import React from "react";
 import Container from "@mui/material/Container";
-import { Typography } from "@mui/material";
+import { Grid, Paper, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [customers, setCustomers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3001/customers");
+      const responseJSON = await response.json();
+
+      setCustomers(responseJSON);
+    };
+
+    fetchData();
+  }, []);
   return (
     <Container maxWidth="lg">
       <Typography variant="h3" gutterBottom align="center">
         App to manage customers
       </Typography>
+      <Grid container p={5} spacing={5}>
+        {customers &&
+          customers.map((customer) => (
+            <Grid item xs={4} key={customer.id}>
+              <Paper>
+                <Typography
+                  variant="h5"
+                  color="initial"
+                  align="center"
+                  gutterBottom
+                >
+                  {customer.name}
+                </Typography>
+                <Typography variant="h6" color="initial" align="justify">
+                  {customer.details}
+                </Typography>
+              </Paper>
+            </Grid>
+          ))}
+      </Grid>
     </Container>
   );
 }
